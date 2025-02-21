12f13d58480004655a1bfd2f966a6c634ec1f6d4
Tamek <khanh.lh06586@sinhvien.hoasen.edu.vn>
2025-02-22 00:09:12 +0700

    Feat: Add form input for booking

--- src/app/pages/book-party-page/book-party-page.component.ts
+++ src/app/pages/book-party-page/book-party-page.component.ts
@@ -2,13 +2,32 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {ErrorStateMatcher, provideNativeDateAdapter} from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FormControl, FormGroupDirective, FormsModule, NgForm, ReactiveFormsModule, Validators} from '@angular/forms';
 @Component({
   selector: 'app-book-party-page',
   standalone: true,
-  imports: [MatButtonModule, MatDividerModule, MatIconModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule,
+    FormsModule, MatFormFieldModule, MatInputModule,ReactiveFormsModule,
+    MatRadioModule,MatDatepickerModule,MatCheckboxModule],
   templateUrl: './book-party-page.component.html',
   styleUrl: './book-party-page.component.scss'
 })
 export class BookPartyPageComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 
+  matcher = new MyErrorStateMatcher();
+}
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
 }
